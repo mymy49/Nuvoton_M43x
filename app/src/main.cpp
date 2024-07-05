8ffa71233c8fd38b495db8c787896cfc628ea57d
Yoon-Ki Hong <mymy49@nate.com>
2024-07-05 12:31:01 +0900

    [DMA Test Code added] - Added examples of how memcpyd() and memsetd() functions work

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -23,6 +23,7 @@
 
 #include <yss.h>
 #include <bsp.h>
+#include <std_ext/string.h>
 
 void thread_blinkLedR(void);
 void thread_blinkLedY(void);
@@ -30,11 +31,28 @@ void thread_blinkLedG(void);
 
 int main(void)
 {
+	uint8_t sa[32], da[32];
+
+	for(uint32_t i = 0; i < 32; i++)
+	{
+		sa[i] = i;
+		da[i] = 0;
+	}
+
 	// 운영체체 초기화
 	initializeYss();
 
 	// 보드 초기화
 	initializeBoard();
+
+	// DMA memory to memory Test
+	memcpyd(da, sa, 32);
+
+	memsetd(da, 0xAA, 32);
+
+	memsethwd(da, 0xBBCC, 16);
+
+	memsetwd(da, 0x12345678, 8);
 
 	thread::add(thread_blinkLedR, 512);
 	thread::add(thread_blinkLedG, 512);
